9a622d4b1f3095d6cdc593c2d91089350a54ba5b
bchiyanike <bchiyanike@gmail.com>
2025-08-09 06:07:37 +0200

    Update CommodityAdapter.java

--- app/src/main/java/com/lionico/mbaremusika/CommodityAdapter.java
+++ app/src/main/java/com/lionico/mbaremusika/CommodityAdapter.java
@@ -4,89 +4,66 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
-import java.util.List;
 
-public class CommodityAdapter extends BaseAdapter {
-    private Context context;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+import java.util.Locale;
+
+public class CommodityAdapter extends RecyclerView.Adapter<CommodityAdapter.ViewHolder> {
+
+    private final Context context;
     private List<Commodity> commodities;
-    private LayoutInflater inflater;
 
     public CommodityAdapter(Context context, List<Commodity> commodities) {
         this.context = context;
         this.commodities = commodities;
-        this.inflater = LayoutInflater.from(context);
+    }
+
+    @NonNull
+    @Override
+    public CommodityAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.list_item_commodity, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
-    public int getCount() {
-        return commodities != null ? commodities.size() : 0;
-    }
+    public void onBindViewHolder(@NonNull CommodityAdapter.ViewHolder holder, int position) {
+        Commodity commodity = commodities.get(position);
+        holder.nameText.setText(commodity.getName());
+        holder.quantityText.setText(commodity.getQuantity());
 
-    @Override
-    public Object getItem(int position) {
-        return commodities != null ? commodities.get(position) : null;
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return position;
-    }
-
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        ViewHolder holder;
-
-        if (convertView == null) {
-            convertView = inflater.inflate(R.layout.list_item_commodity, parent, false);
-            holder = new ViewHolder();
-            holder.nameText = convertView.findViewById(R.id.tv_commodity_name);
-            holder.quantityText = convertView.findViewById(R.id.tv_quantity);
-            holder.usdPriceText = convertView.findViewById(R.id.tv_usd_price);
-            holder.favoriteStar = convertView.findViewById(R.id.iv_favorite);
-            convertView.setTag(holder);
-        } else {
-            holder = (ViewHolder) convertView.getTag();
+        // Format price with $ and 2 decimals
+        try {
+            double price = Double.parseDouble(commodity.getUsdPrice());
+            holder.usdPriceText.setText(String.format(Locale.getDefault(), "$%.2f", price));
+        } catch (NumberFormatException e) {
+            holder.usdPriceText.setText("$" + commodity.getUsdPrice());
         }
 
-        final Commodity commodity = commodities.get(position);
-        if (commodity != null) {
-            holder.nameText.setText(commodity.getName());
-            holder.quantityText.setText(commodity.getQuantity());
+        // Set star icon
+        holder.favoriteStar.setImageResource(
+            commodity.isFavorite() ? R.drawable.ic_star_filled : R.drawable.ic_star_empty
+        );
 
-            // Format USD price with dollar sign
-            String price = commodity.getUsdPrice();
-            if (!price.startsWith("$")) {
-                price = "$" + price;
+        // Favorite toggle click listener
+        holder.favoriteStar.setOnClickListener(v -> {
+            boolean newState = !commodity.isFavorite();
+            commodity.setFavorite(newState);
+            notifyItemChanged(position);
+
+            if (context instanceof MainActivity) {
+                ((MainActivity) context).saveFavorites();
             }
-            holder.usdPriceText.setText(price);
+        });
+    }
 
-            // Update favorite star
-            holder.favoriteStar.setImageResource(
-                commodity.isFavorite() ? 
-                R.drawable.ic_star_filled : 
-                R.drawable.ic_star_empty
-            );
-
-            // Favorite toggle
-            holder.favoriteStar.setOnClickListener(new View.OnClickListener() {
-					@Override
-					public void onClick(View v) {
-						boolean newState = !commodity.isFavorite();
-						commodity.setFavorite(newState);
-						notifyDataSetChanged();
-
-						// Save favorites
-						if (context instanceof MainActivity) {
-							((MainActivity) context).saveFavorites();
-						}
-					}
-				});
-        }
-
-        return convertView;
+    @Override
+    public int getItemCount() {
+        return commodities != null ? commodities.size() : 0;
     }
 
     public void updateData(List<Commodity> newCommodities) {
@@ -94,10 +71,18 @@ public class CommodityAdapter extends BaseAdapter {
         notifyDataSetChanged();
     }
 
-    private static class ViewHolder {
+    static class ViewHolder extends RecyclerView.ViewHolder {
         TextView nameText;
         TextView quantityText;
         TextView usdPriceText;
         ImageView favoriteStar;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            nameText = itemView.findViewById(R.id.tv_commodity_name);
+            quantityText = itemView.findViewById(R.id.tv_quantity);
+            usdPriceText = itemView.findViewById(R.id.tv_usd_price);
+            favoriteStar = itemView.findViewById(R.id.iv_favorite);
+        }
     }
 }
